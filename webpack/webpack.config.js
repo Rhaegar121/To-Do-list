cbe1340fbe13ce671d7dee85d237b14681964062
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-01-17 17:42:35 +0630

    add mode key with value set to development

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: "development",
   entry: './src/index.js',
   devServer: {
     static: './dist',
