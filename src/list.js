b83246a39ece18803bd3240f779dc2f6ccb4571c
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-01-19 01:34:53 +0630

    fix stylelint errors

---
+++ src/list.js
@@ -0,0 +1,9 @@
+class List {
+  constructor(description, index, completed = false) {
+    this.description = description;
+    this.index = index;
+    this.completed = completed;
+  }
+}
+
+export default List;
