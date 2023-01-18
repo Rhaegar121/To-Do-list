97f93ed6304ec73eaff9ae2d87573d096862e013
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-01-18 18:49:09 +0630

    fix stylelint and eslint errors

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,25 +1,23 @@
 import './style.css';
+import Library from './library';
 
+const input = document.querySelector('#input_list');
 const todoList = document.querySelector('#to_do_list');
-const list = [
-  {
-    description: 'wash the dishes',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'complete to do list project',
-    completed: false,
-    index: 2,
-  },
-];
-
-function showList() {
-  list.forEach((i) => {
-    todoList.innerHTML += `<div id="list${i.index}" class="todolist"><input type="checkbox"><input type="text" placeholder="${i.description}" class="listname"><i class="fa-solid fa-ellipsis-vertical"></i></div>`;
-  });
-}
 
 window.onload = () => {
-  showList();
-};
+  const newLibrary = new Library();
+  newLibrary.showList();
+
+  input.onkeypress = (e) => {
+    if (e.keyCode === 13) {
+      newLibrary.addList();
+    }
+  }
+
+  todoList.onclick = (e) => {
+    if (e.target.className.includes('fa-trash-can')) {
+      let data = e.target;
+      newLibrary.removeList(data);
+    } 
+  }
+}
